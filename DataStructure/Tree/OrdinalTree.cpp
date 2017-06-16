32323a6aa6e4f45a7057b408e462f8652f462c85
libra189 <libra189@gmail.com>
2017-06-16 11:53:03 +0900

    順序木構築メソッドの追加

--- DataStructure/Tree/OrdinalTree.cpp
+++ DataStructure/Tree/OrdinalTree.cpp
@@ -7,24 +7,54 @@ using namespace std;
 
 /**
  * ノード挿入
+ * @param  **node 現在のノード
  * @param  key  キー
  * @param  name 名前
- * @return      根ノード
  */
-Node *insertNode(Node *root, int key, string name) {
+Node *insertNode(int key, string name, Node *node) {
+    if (node == NULL) {
+        return new Node(key, name);
+    } else if (key < node->key) {
+        node->left = insertNode(key, name, node->left);
+    } else if (key > node->key) {
+        node->right = insertNode(key, name, node->right);
+    }
+    return node;
+}
 
+/**
+ * 節の削除
+ * @param node 節ノード
+ */
+void deleteNode(Node *node) {
+    if (node != NULL) {
+        deleteNode(node->left);
+        deleteNode(node->right);
+        delete node;
+    }
+}
+
+/**
+ * 木の削除
+ * @param root 根ノード
+ */
+void destroyTree(Node *root) {
+    deleteNode(root);
+    delete root;
 }
 
 int main(int argc, char const *argv[]) {
     // 根ノード
-    Node *root = NULL;
+    static Node *root = NULL;
 
     // 木構造の構築
-    root = insertNode(root, 1, "foo");
-    root = insertNode(root, 2, "bar");
-    root = insertNode(root, 3, "hoge");
-    root = insertNode(root, 4, "test");
+    root = insertNode(5, "foo", root);
+    root = insertNode(1, "bar", root);
+    root = insertNode(2, "hoge", root);
+    root = insertNode(10, "test", root);
     showTree(root);
+
+    destroyTree(root);
 
     return 0;
 }
