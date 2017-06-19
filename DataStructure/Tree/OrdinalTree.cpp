ca55f3324c0a87f7f75e098418bbf0f54dc18e24
libra189 <libra189@gmail.com>
2017-06-19 16:25:49 +0900

    指定したnodeを削除するメソッドの実装

--- DataStructure/Tree/OrdinalTree.cpp
+++ DataStructure/Tree/OrdinalTree.cpp
@@ -23,6 +23,90 @@ Node *insertNode(int key, string name, Node *node) {
 }
 
 /**
+ * 指定したkeyを持つノードを削除
+ * @param root 根ノード
+ * @param key  削除する値
+ */
+void deleteKeyNode(Node *root, int key) {
+    Node *node, *parent;
+    Node *leftBiggest;
+    int direction;
+
+    node = root;
+    parent = NULL;
+    direction = 0;
+
+    // 削除するノードの探査
+    while (node != NULL && node->key != key) {
+        parent = node;
+        if (node->key > key) {
+            node = node->left;
+            direction = -1; // 親の左側
+        } else {
+            node = node->right;
+            direction = 1;  // 親の右側
+        }
+    }
+
+    // 削除ノードが無かった場合
+    if (node == NULL) {
+        return;
+    }
+
+    if (node->left == NULL || node->right == NULL) {
+        /* 左か右，どちらかがNULLであった場合
+           (両方NULLの場合も含む) */
+
+        // 親のポインタを変更
+        if (node->left == NULL) {
+            if (direction == -1) {
+                parent->left = node->right;
+            }
+            if (direction == 1) {
+                parent->right = node->right;
+            }
+            if (direction == 0) {
+                root = node->left;
+            }
+        } else {
+            if (direction == -1) {
+                parent->left = node->left;
+            }
+            if (direction == 1) {
+                parent->right = node->left;
+            }
+            if (direction == 0) {
+                parent = node->left;
+            }
+        }
+
+        delete node;
+    } else {
+        /* 両者ともNULLでなかった場合 */
+
+        // nodeの左側の最も大きな値（最も右側の値）を取得する
+        leftBiggest = node->left;
+        parent = node;
+        direction = -1;
+        while (leftBiggest->right != NULL) {
+            parent = leftBiggest->right;
+            leftBiggest = leftBiggest->right;
+            direction = 1;
+        }
+
+        // left_biggestの値をnodeに代入し、left_biggestは左側の枝を入れる
+        node->key = leftBiggest->key;
+        if (direction == -1) {
+            parent->left = leftBiggest->left;
+        } else {
+            parent->right = leftBiggest->left;
+        }
+
+        delete leftBiggest;
+    }
+}
+
+/**
  * 節の削除
  * @param node 節ノード
  */
@@ -104,6 +188,9 @@ int main(int argc, char const *argv[]) {
     root = insertNode(30, "age", root);
     showTree(root);
     cout << "最大値 : " << searchMaxTree(root) << endl << endl;
+
+    deleteKeyNode(root, 10);
+    showTree(root);
 
     destroyTree(root);
 
