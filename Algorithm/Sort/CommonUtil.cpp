d2c6ecd0a6224542905a08c94b966a299ac2fecd
libra189 <libra189@gmail.com>
2017-06-23 18:04:10 +0900

    共通ユーティリティファイルの追加

---
+++ Algorithm/Sort/CommonUtil.cpp
@@ -0,0 +1,34 @@
+#include <random>
+
+using namespace std;
+
+/**
+ * 乱数生成器
+ * 0-99の範囲で生成
+ * @param  num 生成数
+ * @return     指定された数の乱数配列
+ */
+int *randomizer(const int num) {
+    int *randomList;
+    random_device rnd;
+    mt19937 mt(rnd());
+    uniform_int_distribution<> rand100(0, 99);
+
+    randomList = new int[num];
+    for (int i = 0; i < num; ++i) {
+        randomList[i] = rand100(mt);
+    }
+
+    return randomList;
+}
+
+/**
+ * 配列表示メソッド
+ * @param array 表示したい配列
+ */
+void showArray(int *array, int len) {
+    for (int i = 0;i < len;i++) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+}
