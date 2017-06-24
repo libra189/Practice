11e46d1f95810cb52211b13d814315351f151050
libra189 <libra189@gmail.com>
2017-06-24 21:33:44 +0900

    バブルソートの実装

--- Algorithm/Sort/BubbleSort.cpp
+++ Algorithm/Sort/BubbleSort.cpp
@@ -3,9 +3,25 @@
 
 using namespace std;
 
+int *bubleSort(int *array, int len) {
+    for (int i = 0;i < len-1;i++) {
+        for (int j = 1;j < len - i + 1;j++) {
+            if (array[j] < array[j-1]) {
+                int tmp = array[j];
+                array[j] = array[j-1];
+                array[j-1] = tmp;
+            }
+        }
+    }
+    return array;
+}
+
 int main(int argc, char const *argv[]) {
     const int len = 5;
     int *array = randomizer(len);
+    showArray(array, len);
+
+    array = bubleSort(array, len);
     showArray(array, len);
 
     return 0;
