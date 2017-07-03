5753b6f8c4234696517ae3b0c1286ea92dbc2b19
libra189 <libra189@gmail.com>
2017-07-03 18:04:22 +0900

    クイックソートの実装

--- Algorithm/Sort/QuickSort.cpp
+++ Algorithm/Sort/QuickSort.cpp
@@ -1,18 +1,49 @@
-#include "CommonUtil"
 #include <iostream>
+#include "CommonUtil.cpp"
 
 using namespace std;
 
 /**
  * クイックソート
  * @param array 対象配列
- * @param len   配列の長さ
+ * @param left  要素の最初
+ * @param right 要素の最後
  */
-void quickSort(int *array, int len) {
+void quickSort(int *array, int left, int right) {
+    int i = left;
+    int j = right;
+    int pivot = array[(i + j) / 2];
 
+    while (true) {
+        while (array[i] < pivot) {
+            ++i;
+        }
+        while (pivot < array[j]) {
+            --j;
+        }
+        if (i >= j) break;
+
+        swap(&array[i], &array[j]);
+        i++;
+        j--;
+    }
+
+    if (left < i - 1) {
+        quickSort(array, left, i - 1);
+    }
+    if (j + 1 < right) {
+        quickSort(array, j + 1, right);
+    }
 }
 
 int main(int argc, char const *argv[]) {
-    /* code */
+    const int len = 5;
+    int *array = randomizer(len);
+    showArray(array, len);
+
+    quickSort(array, 0, len - 1);
+    showArray(array, len);
+
+    delete array;
     return 0;
 }
