945369ba22b7621836a32667a69b1c34a702e6ab
libra189 <libra189@gmail.com>
2017-07-28 08:58:54 +0900

    キューの実装

---
+++ DataStructure/List/Queue.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 10;
+int *queue;
+int head;
+int tail;
+
+void initialize() {
+    queue = new int[MAX];
+    head = tail = 0;
+}
+
+bool isFull() { return head == (tail + 1) % MAX; }
+
+bool isEmpty() { return head == tail; }
+
+void enqueue(int x) {
+    if (isFull()) {
+        cerr << "Queue is Full at Error" << endl;
+        return;
+    }
+    queue[tail] = x;
+    if (tail + 1 == MAX) {
+        tail = 0;
+    } else {
+        tail++;
+    }
+}
+
+int dequeue() {
+    if (isEmpty()) {
+        cerr << "Queue is Empth at Error" << endl;
+        return NULL;
+    }
+    int x = queue[head];
+    if (head + 1 == MAX) {
+        head = 0;
+    } else {
+        head++;
+    }
+    return x;
+}
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello World." << endl;
+    return 0;
+}
